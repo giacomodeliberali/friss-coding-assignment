8f061fe1cbe69e1b0b12abcb6f3aff10afe78044
Giacomo De Liberali <deliberali.giacomo@gmail.com>
2021-11-10 15:06:14 +0100

    Add initial migration

---
+++ src/EntityFrameworkCore/Migrations/20211110140253_InitialMigration.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EntityFrameworkCore.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    FirstName = table.Column<string>(type: "TEXT", maxLength: 32, nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", maxLength: 32, nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    IdentificationNumber = table.Column<string>(type: "TEXT", maxLength: 64, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rules.RuleParameters",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 32, nullable: false),
+                    Value = table.Column<decimal>(type: "TEXT", nullable: false),
+                    RuleId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rules.RuleParameters", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rules.Rules",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 32, nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 256, nullable: false),
+                    IsEnabled = table.Column<bool>(type: "INTEGER", nullable: false),
+                    RuleTypeAssemblyQualifiedName = table.Column<string>(type: "TEXT", maxLength: 256, nullable: false),
+                    StrategyId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Order = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rules.Rules", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rules.Strategies",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 32, nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 256, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rules.Strategies", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "People");
+
+            migrationBuilder.DropTable(
+                name: "Rules.RuleParameters");
+
+            migrationBuilder.DropTable(
+                name: "Rules.Rules");
+
+            migrationBuilder.DropTable(
+                name: "Rules.Strategies");
+        }
+    }
+}
